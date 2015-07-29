ee1bcb07b995650a3e1f9edbfb9e8f9e0be1843d
Gina Häußge <osd@foosel.net>
2015-07-29 09:48:04 +0200

    Update check wasn't properly registered

--- octoprint_pbpilink/__init__.py
+++ octoprint_pbpilink/__init__.py
@@ -138,8 +138,8 @@ def __plugin_load__():
 	global __plugin_implementation__
 	__plugin_implementation__ = PbPiLinkPlugin()
 
-	# global __plugin_hooks__
-	# __plugin_hooks__ = {
-	#    "some.octoprint.hook": __plugin_implementation__.some_hook_handler
-	# }
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
 
